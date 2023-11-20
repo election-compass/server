60196aca1968b1f9e2d16f4b4cbeee23ed283c27
Stycks4 <raphael.suliman@globekeeper.com>
2023-11-20 17:50:54 +0200

    ♻️ array to map

--- controllers/general.js
+++ controllers/general.js
@@ -1,43 +1,72 @@
-const Bills = require('../models/Bill');
-const Votes = require('../models/Vote');
-const localUtils = require('../util/localUtils');
+const Bills = require("../models/Bill");
+const Votes = require("../models/Vote");
+const localUtils = require("../util/localUtils");
 
 exports.getBills = async (req, res) => {
-    const { knessetNum  } = req.query;
-    const filter = knessetNum? {knesset_num: knessetNum} : {}; //filter by knesset num if given
-    try {
-        const bills = await Bills.findAll({where: filter, attributes: ['id', 'name']});
-      return res.status(200).json(bills);
-    } catch (error) {
-      return res.status(404).json(error.message);
-    }
+  const { knessetNum } = req.query;
+  const filter = knessetNum ? { knesset_num: knessetNum } : {}; //filter by knesset num if given
+  try {
+    const bills = await Bills.findAll({
+      where: filter,
+      attributes: ["id", "name"],
+    });
+    return res.status(200).json(bills);
+  } catch (error) {
+    return res.status(404).json(error.message);
   }
+};
 
-  exports.postUserVotes = async (req, res) => {
-    const body = req.body;
-    const selectedBills = Object.keys(body);
-    let allVotes = [];
+exports.postUserVotes = async (req, res) => {
+  const body = req.body;
+  const selectedBills = Object.keys(body);
+  const data = new Map();
 
-    try {
-      for (const bill of selectedBills) {
-        const votes = await Votes.findAll({
-          where: { billId: bill },
-          attributes: ['billId', 'mkId', 'mkVote'],
-        });
-  
-        const voteValues = votes.map(vote => ({
-          billId: vote.dataValues.billId,
-          mkId: vote.dataValues.mkId,
-          mkVote: localUtils.voteStringToInt(vote.dataValues.mkVote)
-        }));
+  try {
+    for (const bill of selectedBills) {
+      // fetch all voters associated with current bill
+      const votes = await Votes.findAll({
+        where: { billId: bill },
+        attributes: ["billId", "mkId", "mkVote"],
+      });
+      
+      // map all votes to their voter
+      votes.forEach((vote) => {
+        const { billId, mkId, mkVote } = vote.dataValues;
+        const mkVoteInt = localUtils.voteStringToInt(mkVote);
         
-        allVotes.push(voteValues);
-      }
-      const scores = localUtils.findScoresToMembers(body, allVotes.flat(), selectedBills.length);
-      return res.status(200).json(Object.fromEntries(scores));
-    } catch (error) {
-      console.log(error)
-      return res.status(404).json(error.message);
+        if (data.has(mkId)) {
+          data.get(mkId).push({ billId, mkVote: mkVoteInt });
+          return;
+        }
+        data.set(mkId, [{ billId, mkVote: mkVoteInt }]);
+      });
     }
-  };
-  
+
+    const scores = localUtils.findScoresToMembers(
+      body,
+      data,
+      selectedBills.length
+    );
+    
+    //update data with score and km name
+    try {
+      for (const [mkId, mkScore] of scores) {
+        const existingData = data.get(mkId);
+        data.delete(mkId);
+
+        const mkName = await Votes.findOne({
+          where: { mkId },
+          attributes: ["mkName"],
+        });
+        data.set(mkName.dataValues.mkName, {votes: existingData, score: mkScore})
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    return res.status(200).json(Array.from(data));
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json(error.message);
+  }
+};
--- util/localUtils.js
+++ util/localUtils.js
@@ -19,32 +19,31 @@ exports.voteStringToInt = (vote) => {
     case NEUTRAL_STRING:
       return NEUTRAL_VOTE;
     default:
-        return NEUTRAL_VOTE;
+      return NEUTRAL_VOTE;
   }
 };
 
 /**
  * Finds the score of each Knesset member in relation to what the user chose.
  * @param {Map} userVotes - mapping of IDs for bills and the corresponding votes cast by a user.
- * @param {Object} members_vote_object  dict of {key= bill id, value= list of {member id, vote}}. // vote (-1 0 1)
+ * @param {Map} members_vote_object  mapping of kmIds and their votes for each bill
  * @returns {Map} score per member {key= member id, val= score}.
  */
 exports.findScoresToMembers = (userVotes, members_vote_object, totalBills) => {
   const res = new Map();
   const relativePoint = MAX_GRADE / totalBills;
 
-  members_vote_object.forEach((vote) => {
-    let currentScore = res.get(vote.mkId) || NEUTRAL_SCORE;
+  members_vote_object.forEach((mkData, mkId) => {
+    let currentScore = res.get(mkId) || NEUTRAL_SCORE;
+
     //if the user has voted on current bull
-    if (userVotes[vote.billId] !== NEUTRAL_VOTE) {
+    if (userVotes[mkData.billId] !== NEUTRAL_VOTE) {
       currentScore +=
-        vote.mkVote === userVotes[vote.billId]
-          ? relativePoint
-          : NEUTRAL_SCORE;
+      mkData.mkVote === userVotes[mkData.billId] ? relativePoint : NEUTRAL_SCORE;
     } else {
       currentScore += relativePoint;
     }
-    res.set(vote.mkId, currentScore);
+    res.set(mkId, currentScore);
   });
   return res;
 };
