9f44e9d821d1593dd24a1869dbe1e356cbf8761d
Stycks4 <raphael.suliman@globekeeper.com>
2023-11-21 18:23:08 +0200

    ⚙️ env for sql

--- util/database.js
+++ util/database.js
@@ -1,8 +1,9 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('knesset', 'root', '123456Aa', {
+const sequelize = new Sequelize(process.env.CLOUD_SQL_DATABASE, process.env.CLOUD_SQL_USERNAME, process.env.CLOUD_SQL_PASSWORD, {
   dialect: 'mysql',
-  host: 'localhost'
+  host: process.env.CLOUD_SQL_HOST,
+  port: process.env.CLOUD_SQL_PORT,
 });
 
 module.exports = sequelize;
